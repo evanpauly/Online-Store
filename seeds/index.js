e602bf3e2684cb96ac7180b61f0a6c5dbbaa68fc
rharris529 <jordandine@gmail.com>
2022-03-31 19:55:01 -0500

    added seeds files for routing

---
+++ seeds/index.js
@@ -0,0 +1,17 @@
+const seedProducts = require('./product.seeds');
+const seedCategories = require('./category-seeds');
+
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+    console.log('\n---- DATABASE SYNCED ---- \n');
+    await seedCategories();
+    console.log('\n---- CATEGORIES ADDED --- \n');
+    await seedProducts();
+    console.log('\n---- PRODUCTS ADDED --- \n');
+
+    process.exit(0);
+};
+
+seedAll();
