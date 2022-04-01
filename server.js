de774f916e5ca01d82aa39626bbcef538f5498bd
Cha Vue <chavue09@gmail.com>
2022-03-31 20:02:39 -0500

    materializecss and view/handlebars

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
-const sequelize = require('sequelize');
+// const sequelize = require('sequelize');
+const exphbs = require("express-handlebars");
+// const materialize = require("materialize-css");
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -7,8 +10,16 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Set Handlebars as the default templating engine.
+
+
+// app.engine('handlebars', hbs.engine);
+app.set("view engine", "handlebars");
+
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now Listening'));
 });
+
+
 
