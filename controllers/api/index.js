15a9d1bfb9d7d09b70cfe6e0688943b095bbd7e6
Cha Vue <chavue09@gmail.com>
2022-04-05 18:19:22 -0500

    main handlebars, javascripts, and server

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
+const homeRoutes = require('./home-routes.js');
 const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const userRoutes = require('./user-routes');
 
+router.use('/', homeRoutes);
 router.use('/categories', categoryRoutes);
-router.use('products', productRoutes);
+router.use('/products', productRoutes);
 router.use('/users', userRoutes);
 
 module.exports = router;
