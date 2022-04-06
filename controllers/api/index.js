bb6b94e14f8d850eed2c87253038079f73dca6c0
Cha Vue <chavue09@gmail.com>
2022-04-05 21:38:55 -0500

    added handlebar files

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const homeRoutes = require('./home-routes.js');
+
 const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const userRoutes = require('./user-routes');
 
-router.use('/', homeRoutes);
+
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/users', userRoutes);
