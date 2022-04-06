6d6550172fcbedfc0f47c0a25986cf6423342993
evanpauly <94558036+evanpauly@users.noreply.github.com>
2022-04-05 20:00:34 -0500

    changing back

--- controllers/api/product-routes.js
+++ controllers/api/product-routes.js
@@ -1,2 +1,92 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  // find all categories
+Product.findAll({
+    include: [
+    {
+        model: Product,
+        attributes: { exclude: [ 'category_id' ]}
+    }]
+})
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+  // find one category by its `id` value
+Product.findOne({
+    where: { id: req.params.id },
+    include: [
+    {
+    model: Product
+    }]
+})
+.then(dbCategoryData => {
+    if (!dbCategoryData) {
+    res.status(404).json({ message: 'No category found with this ID.' });
+    return;
+    }
+    res.json(dbCategoryData)
+})
+.catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+});
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+Category.create(req.body)
+.then(dbCategoryData => res.json(dbCategoryData))
+.catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+});
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+Product.update(req.body, {
+    where: {id: req.params.id}
+})
+.then(dbCategoryData => {
+    if(!dbCategoryData) {
+    res.status(404).json({ message: 'No category found with this ID.'});
+    return;
+    }
+    res.json(dbCategoryData);
+})
+.catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+});
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+Product.destroy({
+    where: {
+    id: req.params.id
+    }
+})
+.then(dbCategoryData => {
+    if(!dbCategoryData) {
+    res.status(404).json({ message: 'No category found with this ID.' });
+    return;
+    }
+    res.json(dbCategoryData);
+})
+.catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+});
+});
 
 module.exports = router;
